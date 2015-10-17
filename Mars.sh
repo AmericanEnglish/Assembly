68380650d8d55268a361b76eabd85e1004380ee9
Carlos Barajas <12barajasc@gmail.com>
2015-10-16 22:03:50 -0400

    Made a working Mars.sh script for running programs

--- Mars.sh
+++ Mars.sh
@@ -1,2 +1,4 @@
-java -jar ../Mars/mars.jar [options] name_of_program [more_files] [args]
+#!/bin/bash
+# export 'PATH=$PATH:"$HOME/Mars/' >> ~/.bashrc
+java -cp "$HOME/Documents/Mars" Mars $*
 #../MARS/mars.jar [options] name_of_program [more_files] [args]
