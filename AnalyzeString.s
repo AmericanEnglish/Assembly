e6be602892d58d7beefcd3876779f6f473de5ae8
Carlos Barajas <12barajasc@gmail.com>
2015-10-22 17:28:56 -0400

    Waiting on string byte program

--- AnalyzeString.s
+++ AnalyzeString.s
@@ -1,11 +1,12 @@
 .data 
 buffer:     .space 20 # Max Size
 size:       .word 5 # Max Characters
-compare:    .asciiz "Sample"
+compare:    .asciiz "Yas"
 message:    .asciiz "Do you liek mudkipz? "
 nline:      .asciiz "\n"
 
 .text
+main:
     la $a0, message # Print Message
     li $v0, 4
     syscall
@@ -20,4 +21,18 @@ nline:      .asciiz "\n"
     add $a0, $a1, $zero # Move Length Of String
     li $v0, 1 # Pring Length
     syscall
-     # Do bit by bit comparison for strings Next
+    # Do bit by bit comparison for strings Next
+    li $t9, 0
+    la $a0, buffer 
+    la $a1, compare
+     
+    jal comparestr
+
+comparestr: # Bytewise compare
+    lbu $t0, ($a0)
+    lbu $t1, ($a1)
+    addi $t9, $t9, 1
+    # beq
+
+stringlen: # String Length
+
