ea31dc15db6af7913be5620b03eb310d60e1922c
Carlos Barajas <12barajasc@gmail.com>
2015-10-22 11:56:40 -0400

    Traditional string reading, needs a buffer/read size

--- AnalyzeString.s
+++ AnalyzeString.s
@@ -5,14 +5,18 @@ message:    .asciiz "Do you liek mudkipz? "
 nline:      .asciiz "\n"
 .text
     la $s0, message # Load Message Location
-    add $a0, $s0, $zero
-    la $a1, buffer # Load Buffer location
-    lw $a2, size # Load max ascii size
-    li $v0, 54 # Gather User String
+    la $s1, nline # Load newline
+    la $s2, buffer # Load budder location
+
+    la $a0, ($s0) # Print Message
+    li $v0, 4
     syscall
-    la $a0, ($a1) # Move Usr Str Address
+    la $a0, ($s2) # Gather String
+    lw $a1, size
+    li $v0,8 
+    syscall
     li $v0,4 # Print User String
     syscall
-    add $a0, $a2, $zero # Move Length Of String
+    add $a0, $a1, $zero # Move Length Of String
     li $v0, 1 # Pring Length
     syscall
