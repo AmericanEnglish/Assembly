14057fc0d2d193015e4512b594258dbd139f3870
Carlos Barajas <12barajasc@gmail.com>
2015-10-22 11:58:34 -0400

    Successful argument usage

--- AnalyzeString.s
+++ AnalyzeString.s
@@ -4,18 +4,16 @@ size:       .word 5 # Max Characters
 message:    .asciiz "Do you liek mudkipz? "
 nline:      .asciiz "\n"
 .text
-    la $s0, message # Load Message Location
-    la $s1, nline # Load newline
-    la $s2, buffer # Load budder location
-
-    la $a0, ($s0) # Print Message
+    la $a0, message # Print Message
     li $v0, 4
     syscall
-    la $a0, ($s2) # Gather String
+    la $a0, buffer # Gather String
     lw $a1, size
     li $v0,8 
     syscall
     li $v0,4 # Print User String
+    syscall
+    la $a0, nline # Print newline
     syscall
     add $a0, $a1, $zero # Move Length Of String
     li $v0, 1 # Pring Length
