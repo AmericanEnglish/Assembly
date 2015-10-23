d262b9ab62bf9dd5bf6143b92b3bb03954b983d5
Carlos Barajas <12barajasc@gmail.com>
2015-10-23 14:09:59 -0400

    Completed Find and Fail

--- HnF.s
+++ HnF.s
@@ -1,8 +1,10 @@
 # Hide and Find
 .data
 list:   .space 400
+size: .word 100
 prompt: .asciiz "Number To Hide: "
-com:    .asciiz ", "
+comma:    .asciiz ", "
+loc:    .asciiz "Location: "
 
 .text
 main:
@@ -14,6 +16,12 @@ main:
     syscall
     add $s1, $v0, $zero # Move user int to saved register
     jal hide
+    lw $t9, size # Prep for find
+    li $a0, 0
+    jal find
+    li $v0, 10
+    syscall
+
 
 hide:
     li $a1, 9 # Gather random column
@@ -28,8 +36,31 @@ hide:
     li $t1, 4 # Adjust column value
     mult $t8, $t1
     mflo $t8
-    add $s2, $s1, $t0 # Adjust memory pointer
+    add $s2, $s0, $t0 # Adjust memory pointer
     add $s2, $s2, $t8
     sw $s1, ($s2) # Store the user int here
+    j return
 
 find:
+    lw $t0, ($s0)
+    beq $t0, $s1, pass 
+    addi $a0, $a0, 1
+    addi $s0, $s0, 4
+    bgt $s0, $t9, fail
+    b find
+
+
+pass:
+    # Print row, col
+
+fail:
+    la $a0, loc
+    li $v0, 4
+    syscall
+    li $a0, -1
+    li $v0, 1
+    syscall
+    b return
+
+return:
+    jr $ra
