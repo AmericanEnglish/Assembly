09780d5ae82daca9491b6ad66b4c79a96b78146f
Carlos Barajas <12barajasc@gmail.com>
2015-10-23 13:35:21 -0400

    Finished hide function

--- HnF.s
+++ HnF.s
@@ -21,7 +21,13 @@ hide:
     syscall 
     add $t0, $a0, $zero
     syscall # Gather random column
-    li $t1, 4
+    add $t8, $a0, $zero
+    li $t1, 40 # Adjust row value
     mult $t0, $t1
     mflo $t0
-    li
+    li $t1, 4 # Adjust column value
+    mult $t8, $t1
+    mflo $t8
+    add $s2, $s1, $t0 # Adjust memory pointer
+    add $s2, $s2, $t8
+    sw $s1, ($s2) # Store the user int here
