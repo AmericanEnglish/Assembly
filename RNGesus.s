ab88d532d005e1e9d52d5a3de5a08b0728a4f194
Carlos Barajas <12barajasc@gmail.com>
2015-10-23 22:30:59 -0400

    $v0 contains the read ints not $a0

--- RNGesus.s
+++ RNGesus.s
@@ -10,24 +10,22 @@ main:
     syscall
     li $v0, 5
     syscall
-    add $a2, $a0, $zero
+    add $s0, $v0, $zero
     la $a0, boundary # Gather Upper Bound
     li $v0, 4
     syscall
     li $v0, 5
     syscall
-    add $a1, $a0, $zero
-    li $t0, 0   # Prep for loop
-    li $v0, 42
+    add $a1, $v0, $zero
     jal loop
-    la $a0, nline # Exit
-    li$v0, 4
+    la $a0, nline # exit
+    li $v0, 4
     syscall
-    li $v0, 10
+    li $v0, 10 
     syscall
 
 loop:
-    beq $a2, $t0, return # Check for completion
+    blez $s0, return # Check for completion
     li $v0, 42 # Print random number
     syscall
     li $v0, 1
@@ -35,7 +33,7 @@ loop:
     la $a0, seperator
     li $v0, 4
     syscall
-    addi $t0, $t0, 1 # Increment
+    sub $s0, $s0, 1 # Increment
     b loop
 
 return:
